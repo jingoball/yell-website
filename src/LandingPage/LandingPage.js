3dc1ed205b976b97d5f2ff78f1dd3e4d3e14c339
Nabil Anuar <jingoball@gmail.com>
2020-09-09 19:46:22 +0800

    add logo

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { TopNav } from './TopNav/TopNav';
+import logo from '../assets/yell_icon4.png';
+import styles from './LandingPage.module.css';
 
 export function LandingPage(){
     return(
         <div>
             <TopNav/>
+            <img src={logo} className={styles.logo}  alt='logo'/>
         </div>
     );
 }
