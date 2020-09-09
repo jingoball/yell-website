8f96501eafb6c8b95fea6996d80a4f29170e1be4
Nabil Anuar <jingoball@gmail.com>
2020-09-09 22:33:03 +0800

    add SearchBar with css

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { TopNav } from './TopNav/TopNav';
 import logo from '../assets/yell_icon4.png';
 import styles from './LandingPage.module.css';
+import { SearchBar } from '../SearchBar/SearchBar';
 
 export function LandingPage(){
     return(
         <div>
             <TopNav/>
             <img src={logo} className={styles.logo}  alt='logo'/>
+            <SearchBar/>
         </div>
     );
 }
