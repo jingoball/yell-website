4f6db4d0ecf22f83d465efaec0516ebcbcde0349
Nabil Anuar <jingoball@gmail.com>
2020-09-10 19:51:48 +0800

    modify layout

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { TopNav } from './TopNav/TopNav';
-import logo from '../assets/yell_icon4.png';
+import logo from '../assets/yell_icon5.png';
 import styles from './LandingPage.module.css';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchSuggestions } from './SearchSuggestions/SearchSuggestions';
 
 export function LandingPage(){
     return(
-        <div>
-            <TopNav/>
-            <img src={logo} className={styles.logo}  alt='logo'/>
-            <SearchBar/>
-            <SearchSuggestions/>
+        <div className={styles.landing}>
+            <div className={styles['search-area']}>
+                <TopNav/>
+                <img src={logo} className={styles.logo}  alt='logo'/>
+                <SearchBar/>
+                <SearchSuggestions/>
+            </div>
         </div>
     );
 }
