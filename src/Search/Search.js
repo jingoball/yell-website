081ce93eab76afdecd21c8a64c6c660505fa65f8
Nabil Anuar <jingoball@gmail.com>
2020-09-16 14:30:09 +0800

    sear for businesses with yelp api

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -4,6 +4,9 @@ import { SubNav } from '../Navbar/SubNav/SubNav';
 import { SearchResults } from './SearchResults/SearchResults';
 import { SearchResultSummary } from './SearchResultSummary/SearchResultSummary';
 import useReactRouter from 'use-react-router';
+import {useBusinessSearch} from '../hooks/yelp-api/useBusinessSearch';
+
+
 
 export function Search(){
 
@@ -11,13 +14,14 @@ export function Search(){
     const params = new URLSearchParams(location.search);
     const termParam = params.get('find_desc');
     const locationParam = params.get('find_loc');
+    const [businesses, amountResults, searchParams, setSearchParams] = useBusinessSearch(termParam, locationParam);
 
     return(
         <div>
             <Navbar term={termParam} location={locationParam}/> 
             <SubNav/>
             <SearchResultSummary term={termParam} location={locationParam}/>
-            <SearchResults/>
+            <SearchResults businesses={businesses}/>
         </div>
         
     );
---
+++ src/hooks/yelp-api/config.js
@@ -0,0 +1,6 @@
+export const API_BASE_URL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3';
+
+// FIXME: remove this token
+// NOTE: NEVER SAVE a bearer token inside the frontend code!
+// Bearer Token indicates that the application is not ready.
+export const BEARER_TOKEN = '';
